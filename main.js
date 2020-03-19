553f3316e69ca8f47d4916d3bc8a055a7d434a7e
Marco Acea <marcoacea@Marcos-MacBook-Pro.local>
2020-03-19 02:35:47 -0400

    Touch Ups

---
+++ main.js
@@ -0,0 +1,9 @@
+$('#submit').click(function() {
+  var done_list= document.querySelectorAll('input[type="checkbox"]:checked');
+  var done = done_list.length;
+  var purity_score = 100-done;
+
+  //TODO: Show the score
+  });
+
+//TODO: Make the reset button work
