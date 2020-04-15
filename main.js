1982c80d8ade976eeb322b4c23bef9fef4a85ac4
Marco Acea <marcoacea@Marcos-MacBook-Pro.local>
2020-04-14 20:23:15 -0400

    update

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 $('#submit').click(function() {
-  var done_list= document.querySelectorAll('input[type="checkbox"]:checked');
+  var done_list = document.querySelectorAll('input[type="checkbox"]:checked');
   var done = done_list.length;
-  var purity_score = 100-done;
+  var purity_score = 100 - done;
 
   //TODO: Show the score
-  });
+});
 
 //TODO: Make the reset button work
