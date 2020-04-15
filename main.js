335dc373a6239127ec5b04c77eaa53ea2f86dbd5
Marco Acea <marcoacea@Marcos-MacBook-Pro.local>
2020-04-14 21:39:07 -0400

    HTML/JS Working, Missing Google Ads integration.

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+$(function(){
+  console.log("ready!");
+  $("#toShow").hide();
+});
+
 $('#submit').click(function() {
   var done_list = document.querySelectorAll('input[type="checkbox"]:checked');
   var done = done_list.length;
   var purity_score = 100 - done;
 
-  //TODO: Show the score
+  $('#score').html(purity_score);
+  // Shows the score and hides the checks
+  $("#toShow").show();
+  $("#toHide").hide();
 });
 
-//TODO: Make the reset button work
+
+// On click of the reset button:
+$('#reset').click(function() {
+    $('input:checkbox').prop('checked',false);
+});
